2dbb3a444def5585ae8ff850c8796508d5182784
Adeyinka-Meduoye <88234999+Adeyinka-Meduoye@users.noreply.github.com>
2023-01-24 12:16:42 +0100

    I updated the JS code
    Changes are required for all data inputted to be collected via the browser console.

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
+let form = document.getElementById('form');
 let sendButton = document.getElementById('send');
 let resetButton = document.getElementById('reset');
-let form = document.getElementById('form');
 
 
 form.addEventListener('submit', function (e) {
     e.preventDefault();
 }) 
+
+
+sendButton.addEventListener('click', function (e) {
+    let name = document.getElementById('name');
+    let email = document.getElementById('email');
+    let message = document.getElementById('message');
+    
+    if (name.value == '' || email.value == '') {
+        alert('Ensure you input a value in all fields!');
+      } else {
+        alert('Your message was sent successfully!');
+        console.log(
+          `This form has a name of ${name.value}, email of ${email.value}, and a message of ${message.value}`
+        );
+    
+        name.value = '';
+        email.value = '';
+        message.value = '';
+      }
+})
+
 
 resetButton.addEventListener('click', function () {
     let name = document.getElementById('name');
@@ -15,21 +36,5 @@ resetButton.addEventListener('click', function () {
     name.value = '';
     email.value = '';
     message.value = '';
-})
-
-sendButton.addEventListener('click', function (e) {
-    let name = document.getElementById('name');
-    let email = document.getElementById('email');
-    let message = document.getElementById('message');
-
-    name = name.value;
-    localStorage.setItem('name', name);
-
-    email = email.value;
-    localStorage.setItem('email', email);
-
-    message = message.value;
-    localStorage.setItem('message', message);
-
 })
 
